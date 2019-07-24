4360efa482215ca59ccaa5f8d0e0fb0bfdb07e43
Lohithabalaji <51355988+Lohithabalaji@users.noreply.github.com>
2019-07-24 16:11:31 +0530

    Rename lohi.py to tomy.py

---
+++ tomy.py
@@ -0,0 +1,10 @@
+s,l=map(int,input().split())
+m=list(map(int,input().split()))
+s=[]
+for i in range(l):
+    s.append(list(map(int,input().split())))
+for i in s:
+  su=0
+  for j in range(i[0]-1,i[1]):
+      su=su+m[j]
+  print(su) 
