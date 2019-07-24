3f7c50dd4ecfe6755aed33270915a8487e3776b4
Lohithabalaji <51355988+Lohithabalaji@users.noreply.github.com>
2019-07-24 16:26:00 +0530

    Update sibi.py

--- sibi.py
+++ sibi.py
@@ -1,17 +1,17 @@
-n8,S=map(int,input().split())
-m=list(map(int,input().split()))
-m.sort()
-m.reverse()
-s=S
-t=0
-for i in m:
-    if(s>=i):
-        no=int(s/i)
-        t=t+no
-        s=s-no*i
-    if s==0:
+n8,S8=map(int,input().split())
+m8=list(map(int,input().split()))
+m8.sort()
+m8.reverse()
+s7=S8
+t7=0
+for i in m8:
+    if(s7>=i):
+        no=int(s7/i)
+        t7=t7+no
+        s7=s7-no*i
+    if s7==0:
         break
-if(s==0):
-   print(t)
+if(s7==0):
+   print(t7)
 else:
    print("it's not posiible to select coins in such a way that they sum upto",S)      
